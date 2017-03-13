127a121adb92e6e0d3e7e968fbc10aada4000cc5
Hector Gonzalez <hector.gonzalez@enova.mx>
2017-03-13 15:03:52 -0600

    Componentes reutilizables

--- app.js
+++ app.js
@@ -15,35 +15,37 @@ import {
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import ArtistBox from './ArtistBox';
+
 export default class PlatziMusic extends Component {
   render() {
 
-    const image = 'https://static.pexels.com/photos/163429/surf-outdoor-sports-surfer-surfing-163429.jpeg'
+    const artist = {
+      image: 'https://static.pexels.com/photos/163429/surf-outdoor-sports-surfer-surfing-163429.jpeg',
+      name: 'Surf asdf',
+      likes: 200,
+      comments: 140,
+    }
 
+    /*
+    const image = 'https://static.pexels.com/photos/163429/surf-outdoor-sports-surfer-surfing-163429.jpeg'
     const name = 'Surf'
     const likes = 200
     const comments = 140
-
-    //const myIcon = (<Icon name="rocket" size={30} color="#900" />)
+    */
 
     return (
       <View style={styles.container}>
 
         <View style={styles.imageBox}>
-          <Image style={styles.image} source={{uri: image}} />
-          <View style={styles.info}>
-            <Text style={styles.name}>{name}</Text>
-            <View style={styles.row}>
-              <View style={styles.iconContainer}>
-                <Icon name="md-heart" size={30} color="gray" />
-                <Text style={styles.count}>{likes}</Text>
-              </View>
-              <View style={styles.iconContainer}>
-                <Icon name="md-chatboxes" size={30} color="gray" />
-                <Text style={styles.count}>{comments}</Text>
-              </View>
-            </View>
-          </View>
+          <ArtistBox artist={artist} />
+          <ArtistBox artist={artist} />
+          <ArtistBox artist={artist} />
+          <ArtistBox artist={artist} />
+          <ArtistBox artist={artist} />
+          <ArtistBox artist={artist} />
+          <ArtistBox artist={artist} />
+          <ArtistBox artist={artist} />
         </View>
 
       </View>
@@ -57,38 +59,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
     paddingTop: 50,
   },
-  image:{
-    width: 150,
-    height: 150,
-  },
-  imageBox:{
-    backgroundColor: 'white',
-    flexDirection: 'row',
-  },
-  info:{
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  name:{
-    fontSize: 20,
-    marginTop: 10,
-    color: '#333',
-  },
-  row:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 30,
-    marginTop: 15,
-  },
-  iconContainer:{
-    flex: 1,
-    alignItems: 'center',
-  },
-  count: {
-    color: 'gray',
-  }
 });
 
 AppRegistry.registerComponent('PlatziMusic', () => PlatziMusic);
